2c261978350ef69168129f6b38a4b1e804e7f61f
yoni2k <yoni.k81@gmail.com>
2019-08-13 17:44:14 +0300

    Splitting into a single model execution function that receives input dictionary and returns results

--- mnist.py
+++ mnist.py
@@ -15,26 +15,19 @@ Activation function last layer: softmax
 
 """ Imports """
 
-import numpy as np
 import warnings
 with warnings.catch_warnings():
     warnings.simplefilter("ignore", category=FutureWarning)
     import tensorflow as tf
 import tensorflow_datasets as tfds  # getting MNIST from there
+from timeit import default_timer as timer
+import numpy as np
 
 """ Settings """
 FRACTION_VALIDATE = 0.1
-BATCH_SIZE = 100
-HIDDEN_WIDTH = 100
-NUM_EPOCHS = 5
-
-
-def print_hyperparams():
-    print(f'FRACTION_VALIDATE: {FRACTION_VALIDATE}')
-    print(f'BATCH_SIZE: {BATCH_SIZE}')
-    print(f'HIDDEN_WIDTH: {HIDDEN_WIDTH}')
-    print(f'NUM_EPOCHS: {NUM_EPOCHS}')
-
+BATCH_SIZE = 10000
+HIDDEN_WIDTH = 50
+NUM_EPOCHS = 1
 
 def acquire_data():
     mnist_dataset, mnist_info = tfds.load(name='mnist', with_info=True, as_supervised=True)
@@ -46,7 +39,7 @@ def acquire_data():
 
     print(f"Num train samples: {mnist_info.splits['train'].num_examples}, Num training samples: {mnist_info.splits['test'].num_examples}")
 
-    return mnist_dataset['train'], mnist_dataset['test'], mnist_info
+    return mnist_dataset, mnist_info.splits['train'].num_examples, mnist_info.splits['test'].num_examples
 
 
 def scale_0_to_1(image, label):
@@ -60,29 +53,32 @@ def scale_data(mnist_train_and_valid_orig, mninst_test_orig):
            mninst_test_orig.map(scale_0_to_1())
 
 
-def split_data(train_data, mnist_info):
-    num_validation_samples = tf.cast(FRACTION_VALIDATE * mnist_info.splits['train'].num_examples, tf.int64)
-    print(f"Num validation samples: {num_validation_samples}, Num training samples: {mnist_info.splits['train'].num_examples - num_validation_samples}")
+def split_data(train_data, num_train_valid_examples):
+    num_validation_samples = tf.cast(FRACTION_VALIDATE * num_train_valid_examples, tf.int64)
+    print(f"Num validation samples: {num_validation_samples}, Num training samples: {num_train_valid_examples - num_validation_samples}")
 
     return train_data.take(num_validation_samples), train_data.skip(num_validation_samples)
 
 
-def prepare_data(train_and_valid_data_orig, test_data_orig, mnist_info):
+def prepare_data(mnist_dataset, num_train_valid_examples, num_test_examples):
+    train_valid_data = mnist_dataset['train']
+    test_data  = mnist_dataset['test']
+
     # Scale data
-    train_valid_data = train_and_valid_data_orig.map(scale_0_to_1)
-    test_data = test_data_orig.map(scale_0_to_1)
+    train_valid_data = train_valid_data.map(scale_0_to_1)
+    test_data = test_data.map(scale_0_to_1)
 
     # Shuffle training data
-    train_valid_data = train_valid_data.shuffle(mnist_info.splits['train'].num_examples)
-    test_data = test_data.shuffle(mnist_info.splits['test'].num_examples)
+    train_valid_data = train_valid_data.shuffle(num_train_valid_examples)
+    test_data = test_data.shuffle(num_test_examples)
 
     # Split training and validation data
-    valid_data, train_data = split_data(train_valid_data, mnist_info)
+    valid_data, train_data = split_data(train_valid_data, num_train_valid_examples)
 
     # Batch train data
     train_data = train_data.batch(BATCH_SIZE)
-    valid_data = valid_data.batch(mnist_info.splits['train'].num_examples)  # giving a too big of a number = keep in one batch
-    test_data = test_data.batch(mnist_info.splits['train'].num_examples)  #keep in one batch
+    valid_data = valid_data.batch(num_train_valid_examples)  # giving a too big of a number = keep in one batch
+    test_data = test_data.batch(num_test_examples)  #keep in one batch
 
     valid_inputs, valid_targets = next(iter(valid_data))  # next will load next batch, since only one, will load everything
 
@@ -102,10 +98,25 @@ def prepare_model():
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     return model
 
+def single_model(mnist_data, num_train_valid_examples, num_test_examples, in_dic):
+    out_dic = {}
 
-print_hyperparams()
-train_and_valid_data_orig, test_data_orig, mnist_info = acquire_data()
-train_data, valid_inputs, valid_targets, test_test = prepare_data(train_and_valid_data_orig, test_data_orig, mnist_info)
-model = prepare_model()
-model.fit(train_data, epochs=NUM_EPOCHS, validation_data=(valid_inputs, valid_targets), verbose=2)
+    train_data, valid_inputs, valid_targets, test_test = prepare_data(mnist_data, num_train_valid_examples,
+                                                                      num_test_examples)
+    model = prepare_model()
+
+    start = timer()
+    history = model.fit(train_data, epochs=NUM_EPOCHS, validation_data=(valid_inputs, valid_targets), verbose=2)
+    end = timer()
+    print("Time took to fit: " + str(end - start))
+    print(history.history)
+    print(f'history.history["accuracy"][0]: {history.history["accuracy"][0]}')
+    print(f'history.history["val_accuracy"][0]: {history.history["val_accuracy"][0]}')
+
+    return out_dic
+
+
+mnist_data, num_train_valid_examples, num_test_examples = acquire_data()
+single_model(mnist_data, num_train_valid_examples, num_test_examples, {})
+
 
