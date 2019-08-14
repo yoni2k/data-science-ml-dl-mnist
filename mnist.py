437ec4767c781ba969353f01c2474be0daf814e3
yoni2k <yoni.k81@gmail.com>
2019-08-14 14:35:19 +0300

    Changes for 2nd run: - Added information how long the whole run is running - 2nd run configuration - check best number of layers - 4 and 5 behaved similarly in the last run, trying 3 and 6 to see if they are clearly worse

--- mnist.py
+++ mnist.py
@@ -15,6 +15,7 @@ TODOs:
 - Give different learning rates
 - Do one run of the best inputs
 - Write comments
+- Add 'softmax' function
 
 """
 
@@ -37,26 +38,29 @@ FRACTION_VALIDATE = 0.1
 #ACCURACY_IMPROVEMENT_DELTA = 0.001  # TODO: Leave 0.0001?
 ACCURACY_IMPROVEMENT_DELTA = 0.01  #
 
-#ACCURACY_IMPROVEMENT_PATIENCE = 3  # TODO: Leave 3?
+#ACCURACY_IMPROVEMENT_PATIENCE = 3  # TODO: Leave 3 or is 2 enough?
 ACCURACY_IMPROVEMENT_PATIENCE = 2
 
-# MAX_NUM_EPOCHS = 50
-MAX_NUM_EPOCHS = 15
+# MAX_NUM_EPOCHS = 50  # Probably never need so much, 10-20 is probably enough
+# MAX_NUM_EPOCHS = 15
+MAX_NUM_EPOCHS = 10
 
+# Tried before [100, 1000]
 #batch_sizes = [1, 100, 1000, 10000, 1000000]
 #batch_sizes = [1, 1000, 1000000]
-batch_sizes = [100, 1000]
+batch_sizes = [500]
 
+## Tried before [10, 64],
 #hidden_widths = [1, 2, 4, 8, 16, 32, 64, 128, 256]
 # hidden_widths = [1, 64, 128]
-hidden_widths = [10, 64]
+hidden_widths = [50]
 
-#nums_layers = [2, 3, 4, 5, 10]
+#nums_layers = [2, 3, 4, 5, 6, 10]
 #nums_layers = [2, 4, 6]
-nums_layers = [4, 5]
+nums_layers = [3, 4, 5, 6]
 
 #functions = ['sigmoid', 'tanh', 'relu', 'softmax']
-functions = ['sigmoid', 'tanh']
+functions = ['relu', 'sigmoid', 'tanh']
 
 def acquire_data():
     mnist_dataset, mnist_info = tfds.load(name='mnist', with_info=True, as_supervised=True)
@@ -150,12 +154,12 @@ def single_model(train_data, valid_inputs, valid_targets, in_dic):
 
     actual_num_epochs = len(history.history["val_accuracy"])
 
-    out_dic['Accuracy Validate Last'] = history.history["val_accuracy"][-1].round(4)
     out_dic['Accuracy Validate Best'] = max(history.history['val_accuracy']).round(4)
     out_dic['Train time'] = round(end - start, 3)
     out_dic['Accuracy Validate per Time'] = round(out_dic['Accuracy Validate Best'] / out_dic['Train time'], 4)
     out_dic['Num epochs'] = actual_num_epochs
     out_dic['Average epoch time'] = round((end - start) / actual_num_epochs, 4)
+    out_dic['Accuracy Validate Last'] = history.history["val_accuracy"][-1].round(4)
     out_dic['Accuracy Train Last'] = history.history["accuracy"][-1].round(4)
     out_dic['Accuracy Train Best'] = max(history.history["accuracy"]).round(4)
 
@@ -168,10 +172,11 @@ def do_numerous_loops():
 
     # TODO initiate to better values
     quickest = {'Train time': 10000}
-    shortest = {'Accuracy Validate Best': 0.001}
+    best_accuracy = {'Accuracy Validate Best': 0.001}
     efficient = {'Accuracy Validate per Time': 0.001/10000}
 
     num_model_trainings = 0
+    time_run_started = timer()
 
     for batch_size in batch_sizes:
         in_dic['Batch size'] = batch_size
@@ -183,7 +188,12 @@ def do_numerous_loops():
                 in_dic['Hidden funcs'] = hidden_funcs
                 for hidden_width in hidden_widths:
                     num_model_trainings += 1
-                    print(f'Model {num_model_trainings}: ====================================')
+                    time_running_sec = timer() - time_run_started
+                    print(f'Model {num_model_trainings}, '
+                          f'total time min: {round(time_running_sec / 60, 1)}, '
+                          f'total time hours: {round(time_running_sec / 60 / 60, 2)}: '
+                          f'seconds per model: {round(time_running_sec / num_model_trainings)}'
+                          f'====================================')
                     in_dic['Hidden width'] = hidden_width
                     out_dic = single_model(train_data, valid_inputs, valid_targets, in_dic)
                     result = in_dic.copy()
@@ -192,15 +202,15 @@ def do_numerous_loops():
 
                     if result['Train time'] < quickest['Train time']:
                         quickest = result
-                    if result['Accuracy Validate Best'] > shortest['Accuracy Validate Best']:
-                        shortest = result
+                    if result['Accuracy Validate Best'] > best_accuracy['Accuracy Validate Best']:
+                        best_accuracy = result
                     if result['Accuracy Validate per Time'] > efficient['Accuracy Validate per Time']:
                         efficient = result
 
-                    print(f'CURRENT:   {result}')
-                    print(f'QUICKEST:  {quickest}')
-                    print(f'SHORTEST:  {shortest}')
-                    print(f'EFFICIENT: {efficient}')
+                    print(f'CURRENT:       {result}')
+                    print(f'QUICKEST:      {quickest}')
+                    print(f'BEST ACCURACY: {best_accuracy}')
+                    print(f'EFFICIENT:     {efficient}')
 
     print('+++++++++++++++++++++++++++++++++++++++++++++++++++++')
     print(f'Total number of models trained: {num_model_trainings}')
@@ -209,15 +219,20 @@ def do_numerous_loops():
     print(pf.to_string())
     pf.to_csv("output\\full.csv")
 
+    time_running_sec = timer() - time_run_started
+
     hyperparams = {
-        'num_model_trainings': num_model_trainings,
-        'ACCURACY_IMPROVEMENT_DELTA': ACCURACY_IMPROVEMENT_DELTA,
-        'ACCURACY_IMPROVEMENT_PATIENCE': ACCURACY_IMPROVEMENT_PATIENCE,
-        'MAX_NUM_EPOCHS': MAX_NUM_EPOCHS,
-        'batch_sizes': batch_sizes,
-        'hidden_widths': hidden_widths,
-        'nums_layers': nums_layers,
-        'functions': functions}
+        'Num Model Trainings': num_model_trainings,
+        'Total time minutes': round(time_running_sec / 60, 1),
+        'Total time hours': round(time_running_sec / 60 / 60, 2),
+        'Seconds per model': round(time_running_sec / num_model_trainings),
+        'Accuracy improve delta': ACCURACY_IMPROVEMENT_DELTA,
+        'Accuracy improve patience': ACCURACY_IMPROVEMENT_PATIENCE,
+        'Max Num Epochs': MAX_NUM_EPOCHS,
+        'Batch sizes': batch_sizes,
+        'Hidden Widths': hidden_widths,
+        'Nums layers': nums_layers,
+        'Functions': functions}
 
     pf = pd.DataFrame([hyperparams])
     print(f'HYPERPARAMS:')
@@ -226,12 +241,12 @@ def do_numerous_loops():
 
     quickest_with_type = {'Type': 'QUICKEST'}
     quickest_with_type.update(quickest)
-    shortest_with_type = {'Type': 'SHORTEST'}
-    shortest_with_type.update(quickest)
+    best_accuracy_with_type = {'Type': 'BEST ACCURACY'}
+    best_accuracy_with_type.update(best_accuracy)
     efficient_with_type = {'Type': 'EFFICIENT'}
-    efficient_with_type.update(quickest)
+    efficient_with_type.update(efficient)
 
-    pf = pd.DataFrame([quickest_with_type, shortest_with_type, efficient_with_type])
+    pf = pd.DataFrame([quickest_with_type, best_accuracy_with_type, efficient_with_type])
     print(f'BEST RESULTS:')
     print(pf.to_string())
     pf.to_csv("output\\best.csv")
